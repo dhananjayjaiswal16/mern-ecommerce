d076e53c1bd509b8f25c9d30ee76a26ff4ff042f
Dhananjay <dhananjayman11@gmail.com>
2021-10-27 00:36:33 +0530

    slide layout

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
+import slideImg1 from '../images/slider-home-img.jpeg'
 
 const Container = styled.div`
+    overflow-x: hidden;
     width: 100%;
     height: calc(100vh - 88px);
-    background: turquoise;
     position: relative;
     display: flex ;
 `
 
 const Arrow = styled.div`
-    background-color: #fff7f7;
-    height: 40px;
-    width: 40px;
+    height: 45px;
+    width: 45px;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -26,16 +26,102 @@ const Arrow = styled.div`
 `
 const Wrapper = styled.div`
     height: 100%;
+    display: flex;
+    justify-content: center;
+    
+`
+
+const Slide = styled.div`
+    display: flex;
+    align-items: center;
+    width: 100vw;
+    height: 100%;
+    background-color: ${props => props.bg};
+`
+
+const ImageContainer = styled.div`
+    flex: 1;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+const Image = styled.img`
+    height: 80%;
+`
+
+const InfoContainer = styled.div`
+    flex: 1;
+`
+const Title = styled.h1`
+    font-size: 60px;
+`
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 18px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    border: 2px solid black;
+    background-color: transparent;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+    &:hover{
+        background-color: teal;
+        color: #fff;
+        border: 2px solid teal;
+    }
 `
 
 const Slider = () => {
     return (
         <Container>
             <Arrow direction='left'>
-                <i className="fas fa-caret-left" />
+                <i className="fas fa-caret-left" style={{ fontSize: '20px' }} />
             </Arrow>
+            <Wrapper>
+                <Slide bg='#f5fafd'>
+                    <ImageContainer>
+                        <Image src={slideImg1}>
+
+                        </Image>
+                    </ImageContainer>
+                    <InfoContainer>
+                        <Title>Summer Sale</Title>
+                        <Desc>Get your favourite products at 30% discount!!</Desc>
+                        <Button>Shop Now</Button>
+                    </InfoContainer>
+                </Slide>
+                <Slide bg='#f5fafd'>
+                    <ImageContainer>
+                        <Image src={slideImg1}>
+
+                        </Image>
+                    </ImageContainer>
+                    <InfoContainer>
+                        <Title>Summer Sale</Title>
+                        <Desc>Get your favourite products at 30% discount!!</Desc>
+                        <Button>Shop Now</Button>
+                    </InfoContainer>
+                </Slide>
+                <Slide bg='#f5fafd'>
+                    <ImageContainer>
+                        <Image src={slideImg1}>
+
+                        </Image>
+                    </ImageContainer>
+                    <InfoContainer>
+                        <Title>Summer Sale</Title>
+                        <Desc>Get your favourite products at 30% discount!!</Desc>
+                        <Button>Shop Now</Button>
+                    </InfoContainer>
+                </Slide>
+            </Wrapper>
             <Arrow direction='right'>
-                <i className="fas fa-caret-right" />
+                <i className="fas fa-caret-right" style={{ fontSize: '20px' }} />
             </Arrow>
         </Container>
     )
