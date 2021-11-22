5826f06dd75582c8da345aa97724b66099a75736
Dhananjay <dhananjayman11@gmail.com>
2021-11-23 00:06:08 +0530

    product by category

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import styled from 'styled-components';
 import { productsData } from '../sampleData'
 import Product from './Product';
@@ -11,7 +12,25 @@ const Container = styled.div`
     flex-wrap: wrap;
 `
 
-const Products = () => {
+const Products = ({ cat, filter, sort }) => {
+    const [products, setProducts] = useState([]);
+
+
+
+    const url = cat
+        ? `http://localhost:8000/api/product?category=${cat}`
+        : 'http://localhost:8000/api/product';
+    useEffect(() => {
+        const getProducts = async () => {
+            try {
+                const res = await axios.get(url);
+                setProducts(res.data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        getProducts();
+    }, [cat])
     return (
         <Container>
             {productsData.map((product) => (
