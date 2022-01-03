10418baf2e9436cdae380259a19b0d8d4b6b005e
Dhananjay <dhananjayman11@gmail.com>
2022-01-03 12:59:17 +0530

    feat: user slice for login

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from './cartSlice';
+import userSlice from './userSlice';
 
 
 export default configureStore({
-    reducer: {
-        cartSlice
-    },
+  reducer: {
+    cartSlice,
+    userSlice
+  },
 })
