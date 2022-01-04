a97e607b862b4013d8f7700ce75c0d94a840be95
Dhananjay <dhananjayman11@gmail.com>
2022-01-04 18:59:15 +0530

    add redux persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from './cartSlice';
 import userSlice from './userSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, userSlice);
 
 
 export default configureStore({
   reducer: {
     cartSlice,
-    userSlice
+    userSlice: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
   },
 })
+
+export let persistor = persistStore(configureStore);
