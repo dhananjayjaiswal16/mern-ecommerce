5304c0e1c78b3f5aeddbe45b380a71a964696a7f
Dhananjay <dhananjayman11@gmail.com>
2022-01-06 12:47:49 +0530

    fix: user request with req headers

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -2,22 +2,16 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:8000/api/";
 
-
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOGI3MmVkNjY4MDE0ZWFkODY4MmJmMSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNzkxNDYxNSwiZXhwIjoxNjM4MTczODE1fQ.10KViBtp410yGZP6cmh9AlG1DUF_tryZYReAmFnhAec';
-
+var TOKEN = '';
+if (localStorage.getItem("persist:root")) {
+  TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.userSlice)?.currentUser?.token;
+}
+console.log("TOKEN in req method", TOKEN);
 export const publicRequest = axios.create({
-    baseURL: BASE_URL,
+  baseURL: BASE_URL,
 });
 
-// const setAuthToken = token => {
-//     if (token) {
-//         axios.defaults.headers.common['x-auth-token'] = token;
-//     } else {
-//         delete axios.defaults.headers.common['x-auth-token'];
-//     }
-// };
-// setAuthToken(TOKEN);
 export const userRequest = axios.create({
-    baseURL: BASE_URL,
+  baseURL: BASE_URL,
+  headers: { token: TOKEN },
 });
-
