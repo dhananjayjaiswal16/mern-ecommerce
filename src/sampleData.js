71e43ce1d8889060cc09e41f2d1044d748ab1d2e
Dhananjay <dhananjayman11@gmail.com>
2022-01-19 10:30:27 +0530

    data changes

--- src/sampleData.js
+++ src/sampleData.js
@@ -7,81 +7,81 @@ import categoryImg2 from './images/category-img2.jpeg'
 import categoryImg3 from './images/category-img3.jpeg'
 
 export const sliderItems = [
-    {
-        id: 1,
-        img: slideImg1,
-        title: "SUMMER SALE",
-        desc: "GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        bg: "#f5fafd",
-    },
-    {
-        id: 2,
-        img: slideImg2,
-        title: "AUTUMN COLLECTION",
-        desc: "GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        bg: "#fcf1ed",
-    },
-    {
-        id: 3,
-        img: slideImg3,
-        title: "WINTER LOVE",
-        desc: "GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        bg: "#fbf0f4",
-    },
+  {
+    id: 1,
+    img: slideImg1,
+    title: "SUMMER SALE",
+    desc: "GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "#f5fafd",
+  },
+  {
+    id: 2,
+    img: slideImg2,
+    title: "AUTUMN COLLECTION",
+    desc: "GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "#fcf1ed",
+  },
+  {
+    id: 3,
+    img: slideImg3,
+    title: "WINTER LOVE",
+    desc: "GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "#fbf0f4",
+  },
 ];
 
 export const categories = [
-    {
-        id: 1,
-        img: categoryImg2,
-        title: "SHIRT STYLE!",
-        cat: "shirt",
-    },
-    {
-        id: 2,
-        img: categoryImg1,
-        title: "WINTER LOVE",
-        cat: "glasses",
-    },
-    {
-        id: 3,
-        img: categoryImg3,
-        title: "LIGHT JACKETS",
-        cat: "jacket",
-    },
+  {
+    id: 1,
+    img: categoryImg2,
+    title: "SHIRT STYLE!",
+    cat: "shirt",
+  },
+  {
+    id: 2,
+    img: categoryImg1,
+    title: "WOMEN LOVE",
+    cat: "female",
+  },
+  {
+    id: 3,
+    img: categoryImg3,
+    title: "LIGHT DRESSES",
+    cat: "dress",
+  },
 ];
 
 export const productsData = [
-    {
-        id: 1,
-        img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
-    },
-    {
-        id: 2,
-        img: "https://img.freepik.com/free-photo/running-shoes-white-background_10541-635.jpg?size=626&ext=jpg",
-    },
-    {
-        id: 3,
-        img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
-    },
-    {
-        id: 4,
-        img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
-    },
-    {
-        id: 5,
-        img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
-    },
-    {
-        id: 6,
-        img: "https://media.istockphoto.com/photos/trucker-hat-or-mesh-cap-isolated-picture-id1163958328?k=20&m=1163958328&s=612x612&w=0&h=6rMHN6S1Er0lzjQmi9KTOeCC58pKcGDt9T8S3cg1-Bo=",
-    },
-    {
-        id: 7,
-        img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
-    },
-    {
-        id: 8,
-        img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
-    },
+  {
+    id: 1,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+  },
+  {
+    id: 2,
+    img: "https://img.freepik.com/free-photo/running-shoes-white-background_10541-635.jpg?size=626&ext=jpg",
+  },
+  {
+    id: 3,
+    img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+  },
+  {
+    id: 4,
+    img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+  },
+  {
+    id: 5,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 6,
+    img: "https://media.istockphoto.com/photos/trucker-hat-or-mesh-cap-isolated-picture-id1163958328?k=20&m=1163958328&s=612x612&w=0&h=6rMHN6S1Er0lzjQmi9KTOeCC58pKcGDt9T8S3cg1-Bo=",
+  },
+  {
+    id: 7,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+  },
+  {
+    id: 8,
+    img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+  },
 ]
